8082b1436575e73b76385b8c1c01d583e3592e7e
Ylana Oliveira <ylanalmo@gmail.com>
2024-06-28 19:33:44 -0300

    setup models controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_28_215841) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_28_220811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "speciality"
+    t.string "description"
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
