87e7602d87157b36fb8c3be7d04a415d385be7f4
Ylana Oliveira <ylanalmo@gmail.com>
2024-07-22 19:17:40 -0300

    ngrok config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << /\A[a-z\d-]+\.ngrok-free\.app\z/
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
